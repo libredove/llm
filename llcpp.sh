f8bc8585cab94cc786915c120dec6e0d8313265e
Michael Libredove <158617202+libredove@users.noreply.github.com>
2024-11-22 22:53:50 +0100

    Update llcpp.sh

--- llcpp.sh
+++ llcpp.sh
@@ -2,6 +2,6 @@
 # Load and run the model:
 llama-cli \
   --hf-repo "bartowski/Llama-3.2-3B-Instruct-uncensored-GGUF" \
-  --hf-file Llama-3.2-3B-Instruct-uncensored-IQ4_XS.gguf \
+  --hf-file  Llama-3.2-3B-Instruct-uncensored-Q4_K_M.gguf \
   -p "you're an extremely kinky and dumb feminine twink. you aren't flamboyant, nor annoying. you're reserved but craving for sex." \
   --conversation
